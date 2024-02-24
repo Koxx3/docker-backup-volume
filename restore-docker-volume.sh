1b71c55ae65576be2a5b1e7cbed7759edab0946c
koxx33@gmail.com <koxx33@gmail.com>
2024-02-24 13:49:33 +0100

    fix

--- restore-docker-volume.sh
+++ restore-docker-volume.sh
@@ -1,23 +1,17 @@
 #!/bin/sh
 
-# STEPS:
-# 1. Validate that the docker-volume doesn't exist and that the restore file (tar.gz) exists
-# 2. Start a new container that mounts the docker-volume and the restore file
-# 3. Extract the content of the restore file to the docker-volume
-# 4. A new container can now mount the restored docker-volume
-
 DOCKER_RESTORE_VOLUME=$1
 RESTORE_FILE=$2
 DOCKER_IMAGE=alpine
 
-function validateInput() {
-    if [ ! -f "${RESTORE_FILE}" ] ; then
+validateInput() {
+    if [ ! -f "${RESTORE_FILE}" ]; then
         echo "> Error: Restore file not found at ${RESTORE_FILE}"
         exit 1
     fi
 
     INSPECT_VOLUME=$(docker volume inspect ${DOCKER_RESTORE_VOLUME} 2>&1)
-    if [[ ! ${INSPECT_VOLUME} == *"No such volume"* ]] ; then
+    if [[ ! ${INSPECT_VOLUME} == *"No such volume"* ]]; then
         echo "> Error: docker volume '${DOCKER_RESTORE_VOLUME}' already exists"
         exit 1
     fi
@@ -28,7 +22,7 @@ validateInput
 
 echo "> Creating docker volume:" $(docker volume create --name ${DOCKER_RESTORE_VOLUME})
 
-docker run --rm	\
+docker run --rm \
     -v ${DOCKER_RESTORE_VOLUME}:/backup-dest \
     -v ${RESTORE_FILE}:/restore-src.tar.gz ${DOCKER_IMAGE} \
     tar -xzvf /restore-src.tar.gz -C /backup-dest
